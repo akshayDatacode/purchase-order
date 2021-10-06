e7dcd6b094699a5fb7fc569776b65702dda9910a
miss-shree <dhanshreenirgude80@gmail.com>
2021-10-06 18:41:09 +0530

    add status handler apis

--- src/apiServices.js
+++ src/apiServices.js
@@ -5,6 +5,7 @@ export const addOrder = (order) => {
   return axios
     .post(`${api}/add_order`, order)
     .then(({ data }) => {
+      getOrders();
       return { success: true, data };
     })
     .catch((error) => {
@@ -20,5 +21,16 @@ export const getOrders = () => {
     })
     .catch((error) => {
       console.log("get orders error", error);
+    });
+};
+
+export const confirmOrder = (data, id) => {
+  return axios
+    .put(`${api}/update_order/${id}`, data)
+    .then(({ data }) => {
+      return { success: true, data };
+    })
+    .catch((error) => {
+      console.log("get confirm_orders error", error);
     });
 };
